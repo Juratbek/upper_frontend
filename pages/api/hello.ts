47f29b8b398c432cda0546b4dbb0947185bb16e4
Samandar <boymurodovuzcoder@gmail.com>
2022-06-25 13:09:18 +0500

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 type Data = {
-  name: string;
+  name: number;
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>): void {
