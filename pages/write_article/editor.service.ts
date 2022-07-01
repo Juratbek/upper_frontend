0766632f4bdfd13fb44e843399b9ddbf77d0b226
Samandar <boymurodovuzcoder@gmail.com>
2022-07-01 17:43:37 +0500

    13:feature/Added editorjs libraries

---
+++ pages/write_article/editor.service.ts
@@ -0,0 +1,5 @@
+import EditorJs from '@editorjs/editorjs';
+import Embed from '@editorjs/embed';
+import Header from '@editorjs/header';
+import ImageTool from '@editorjs/image';
+import List from '@editorjs/list';
