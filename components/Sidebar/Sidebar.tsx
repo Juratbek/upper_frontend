eea94271fb97e47d950e2a73edd0f6a86b731cf8
makhajur <macbookpro@macbooks-MacBook-Pro.local>
2022-07-07 10:18:38 +0500

    28: feature/adding sing in and login buttons

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,5 +1,14 @@
+import { Button } from 'components';
+
 import classes from './Sidebar.module.css';
 
 export const Sidebar = (): JSX.Element => {
-  return <div className={classes.sidebar}>Sidebar</div>;
+  return (
+    <div className={classes.sidebar}>
+      <div className='d-flex justify-content-around'>
+        <Button color='outline-dark'>Kirish</Button>
+        <Button className='float-right'>Ro`yxatdan o`tish</Button>
+      </div>
+    </div>
+  );
 };
