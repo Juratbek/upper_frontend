84b4ab16eb279fc5442859c9158a8973d78fadf3
makhajur <macbookpro@macbooks-MacBook-Pro.local>
2022-07-07 09:34:18 +0500

    28: feature/creating SidebarArticle component

---
+++ components/SidebarArticle/SidebarArticle.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+import { ISidebarArticleProps } from './SidebarArticle.types';
+
+export const SidebarArticle: FC<ISidebarArticleProps> = () => {
+  return <div>sidebar article</div>;
+};
