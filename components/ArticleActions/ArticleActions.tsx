d4d0f9706f6b36599cc8e5b0f3fe8743c520cc94
Samandar <boymurodovuzcoder@gmail.com>
2022-07-19 01:17:17 +0500

    34: feat/Hide actions bar when scrolling down

--- components/ArticleActions/ArticleActions.tsx
+++ components/ArticleActions/ArticleActions.tsx
@@ -1,4 +1,4 @@
-import { RefObject, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { RefObject, useCallback, useEffect, useRef, useState } from 'react';
 import { ICON_TYPES, ICONS } from 'variables';
 
 import styles from './ArticleActions.module.scss';
@@ -11,9 +11,13 @@ interface IArticleActions {
   containerRef: RefObject<HTMLDivElement>;
 }
 
+let lastScrollTop = 0;
+
 export const ArticleActions: React.FC<IArticleActions> = ({ containerRef }: IArticleActions) => {
-  const actionsContainer = useRef<HTMLDivElement>(null);
   const [actionsBarLeft, setActionsBarLeft] = useState<number>(0);
+  const [isScrollingUp, setIsScrollingUp] = useState<boolean>(false);
+
+  const actionsContainer = useRef<HTMLDivElement>(null);
 
   const positionActionsBar = useCallback((): void => {
     if (containerRef.current && actionsContainer.current) {
@@ -27,22 +31,33 @@ export const ArticleActions: React.FC<IArticleActions> = ({ containerRef }: IArt
     }
   }, []);
 
+  const detectScrollDirection = (): void => {
+    const st = document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      setIsScrollingUp(false);
+    } else {
+      setIsScrollingUp(true);
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  };
+
   useEffect(() => {
     positionActionsBar();
 
     window.addEventListener('resize', positionActionsBar);
+    window.addEventListener('scroll', detectScrollDirection);
   }, [containerRef]);
 
   return (
     <div
-      className={styles.articleActions}
+      className={`${styles.articleActions}${isScrollingUp ? ' ' + styles.scrollUp : ''}`}
       ref={actionsContainer}
       style={{ left: actionsBarLeft + 'px' }}
     >
       <div className={styles.iconsContainer}>
         <CommentIcon />
         <LikeIcon />
-        <span>+14k</span>
+        <span className={styles.reactionsText}>+14k</span>
         <DislikeIcon />
         <ShareIcon />
       </div>
