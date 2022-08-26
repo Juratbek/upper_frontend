1dae7b9a90902bce6c448e83c70240851226acf6
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-08-26 19:03:28 +0500

    72:feature/implementing home page tabs

---
+++ frontends/home/ForYouTab/ForYouTab.tsx
@@ -0,0 +1,90 @@
+import { Article } from 'components';
+import { FC } from 'react';
+import { IArticleResult, IBlog, ILabel } from 'types';
+import { SEARCH_PAGE_ARTICLE_ACTIONS, SEARCH_PAGE_ARTICLE_ICONS } from 'variables';
+import { ARTICLE_STATUSES } from 'variables/article';
+
+const author: IBlog = {
+  id: 1,
+  name: 'Samandar',
+  imgUrl: 'awda',
+};
+
+const labels: ILabel[] = [
+  {
+    name: 'JavaScript',
+    id: 1,
+  },
+  {
+    name: 'TypeScript',
+    id: 2,
+  },
+];
+
+const articles: IArticleResult[] = [
+  {
+    id: 1,
+    title: 'Article title',
+    author,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised",
+    labels,
+    publishedDate: new Date(),
+    updatedDate: new Date(),
+    viewCount: 12000,
+    status: ARTICLE_STATUSES.PUBLISHED,
+  },
+  {
+    id: 2,
+    title: 'Article title',
+    author,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised",
+    labels,
+    publishedDate: new Date(),
+    updatedDate: new Date(),
+    viewCount: 12000,
+    status: ARTICLE_STATUSES.PUBLISHED,
+  },
+  {
+    id: 3,
+    title: 'Article title',
+    author,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised",
+    labels,
+    publishedDate: new Date(),
+    updatedDate: new Date(),
+    viewCount: 12000,
+    status: ARTICLE_STATUSES.PUBLISHED,
+  },
+  {
+    id: 4,
+    title: 'Article title',
+    author,
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised",
+    labels,
+    publishedDate: new Date(),
+    updatedDate: new Date(),
+    viewCount: 12000,
+    status: ARTICLE_STATUSES.PUBLISHED,
+  },
+];
+
+export const ForYouTab: FC = () => {
+  return (
+    <div className='tab'>
+      {articles.map((article) => (
+        <Article
+          className='px-2 py-2'
+          key={article.id}
+          article={article}
+          author={article.author}
+          actions={SEARCH_PAGE_ARTICLE_ACTIONS}
+          icons={SEARCH_PAGE_ARTICLE_ICONS}
+        />
+      ))}
+    </div>
+  );
+};
