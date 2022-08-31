e6fee8677032c50ba9d4aa022f5df127e1921912
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-08-31 21:39:02 +0500

    81:fix/resolving two file dependence each other bug

--- store/api/api.constants.ts
+++ store/api/api.constants.ts
@@ -1,9 +1,1 @@
-import { articleApi } from '../article/article.api';
-import { blogApi } from '../blog/blog.api';
-
 export const BASE_URL = 'https://backend.uper.uz';
-
-export const REDUCER_PATHS: Record<string, string> = {
-  blog: blogApi.reducerPath,
-  article: articleApi.reducerPath,
-};
