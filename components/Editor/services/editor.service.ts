f006ec7ae9b611952959d2fba74bb0c0aec763c5
Samandar Boymurodov <boymurodovuzcoder@gmail.com>
2022-09-04 00:23:21 +0500

    65: Moved tools object to separate file

--- components/Editor/services/editor.service.ts
+++ components/Editor/services/editor.service.ts
@@ -1,6 +1,7 @@
 import EditorJs from '@editorjs/editorjs';
 
 import { EDITOR_HOLDER, EDITOR_PLACEHOLDER, IEditorProps } from '../editor.types';
+import { getTools } from './editor-tools';
 
 export const createEditor = async ({
   holder = EDITOR_HOLDER,
@@ -9,107 +10,15 @@ export const createEditor = async ({
   content = { blocks: [] },
   placeholder = EDITOR_PLACEHOLDER,
 }: IEditorProps = {}): Promise<EditorJs> => {
-  // TODO: Move tools to separate file
-  const EditorJs = (await import('@editorjs/editorjs')).default;
-  const Embed = (await import('@editorjs/embed')).default;
-  const Header = (await import('@editorjs/header')).default;
-  const ImageTool = (await import('@editorjs/image')).default;
-  const List = (await import('@editorjs/list')).default;
-  const Quote = (await import('@editorjs/quote')).default;
-  const Delimeter = (await import('@editorjs/delimiter')).default;
-  const Tooltip = (await import('editorjs-tooltip')).default;
-  const TextColor = (await import('editorjs-text-color-plugin')).default;
-  const Alert = (await import('editorjs-alert')).default;
-  const Unsplash = (await import('editorjs-inline-image')).default;
-  const InclineCode = (await import('@editorjs/inline-code')).default;
-  const CodeFlask = (await import('@calumk/editorjs-codeflask')).default;
+  const editorJS = (await import('@editorjs/editorjs')).default;
+  const tools = await getTools();
 
-  return new EditorJs({
-    holder: holder,
+  return new editorJS({
+    holder,
     onChange: changeHandler,
     readOnly: !editable,
-    placeholder: placeholder,
+    placeholder,
     data: content,
-    tools: {
-      header: {
-        class: Header,
-        shortcut: 'CMD+SHIFT+H',
-        config: {
-          levels: [1, 2, 3, 4, 5, 6],
-          defaultLevel: 1,
-        },
-      },
-      alert: Alert,
-      quote: Quote,
-      delimeter: Delimeter,
-      list: List,
-      unsplash: {
-        class: Unsplash,
-        config: {
-          embed: {
-            display: false,
-          },
-          unsplash: {
-            appName: 'udas',
-            clientId: 'YUiELidZbnKRhMEECKChvST2BMHOfCR6X3mPia5ZdbU',
-          },
-        },
-      },
-      image: ImageTool,
-      code: CodeFlask,
-      embed: {
-        class: Embed,
-        inlineToolbar: true,
-        config: {
-          services: {
-            youtube: true,
-            codepen: true,
-            pinterest: {
-              regex: /https:\/\/www\.pinterest.com\/pin\/(.*)\//,
-              embedUrl: 'https://assets.pinterest.com/ext/embed.html?id=<%= remote_id %>',
-              html: '<iframe scrolling="yes" frameborder="no" allowtransparency="true" allowfullscreen="true" style="width: 100%; min-height: 500px; max-height: 1000px;" class="embed-tool__content"></iframe>',
-              height: 500,
-              width: 600,
-            },
-            carbon: {
-              regex: /https?:\/\/carbon.now.sh\/(.*)/,
-              embedUrl: 'https://carbon.now.sh/embed<%= remote_id %>',
-              html: "<iframe height='350' scrolling='yes' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%'></iframe>",
-              height: 350,
-              width: 600,
-            },
-            codesandbox: {
-              regex: /https?:\/\/codesandbox.io\/s\/(.*)/,
-              embedUrl: 'https://codesandbox.io/embed/<%= remote_id %>',
-              html: "<iframe style='width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;' title='frosty-snowflake-4lbhkx' allow='accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking' sandbox='allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts'></iframe>",
-              height: 500,
-              width: 600,
-            },
-            jsfiddle: {
-              regex: /https?:\/\/jsfiddle.net\/(.*)/,
-              embedUrl: 'https://jsfiddle.net/<%= remote_id %>embedded/',
-              html: "<iframe width='100%' height='300' allowfullscreen='allowfullscreen' allowpaymentrequest frameborder='0'></iframe>",
-              height: 500,
-              width: 600,
-            },
-          },
-        },
-      },
-      tooltip: {
-        class: Tooltip,
-        inlineToolbar: true,
-        config: {
-          underline: true,
-        },
-      },
-      textColor: {
-        class: TextColor,
-        inlineToolbar: true,
-      },
-      inlineCode: {
-        class: InclineCode,
-        inlineToolbar: true,
-      },
-    },
+    tools,
   });
 };
