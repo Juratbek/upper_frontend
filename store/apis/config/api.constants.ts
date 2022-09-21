60db40f08c995dc27982cd4de7a3fa8fcb55a37c
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-09-21 17:19:47 +0500

    107:fix/setting base url for deployed server

--- store/apis/config/api.constants.ts
+++ store/apis/config/api.constants.ts
@@ -1,5 +1,5 @@
 // Development
-export const BASE_URL = 'http://localhost:8080';
+// export const BASE_URL = 'http://localhost:8080';
 
 // Production
-// export const BASE_URL = 'https://powerful-brushlands-00932.herokuapp.com';
+export const BASE_URL = 'https://powerful-brushlands-00932.herokuapp.com';
