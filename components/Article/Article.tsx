e14d2566e1245272fe61431aee534c55195c074f
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-09-23 11:58:29 +0500

    119:feature/fetch main page suggested articles

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -1,20 +1,12 @@
-import { Actions, ArticleImg, Author, Label } from 'components';
+import { ArticleImg, Author, Label } from 'components';
 import Link from 'next/link';
 import { FC } from 'react';
 import { formatToKMB, getClassName, toDateString } from 'utils';
-import { ICONS } from 'variables';
 
 import classes from './Article.module.scss';
 import { IArticleProps } from './Article.types';
 
-export const Article: FC<IArticleProps> = ({
-  className,
-  actions,
-  icons,
-  article,
-  author,
-  redirectUrl,
-}) => {
+export const Article: FC<IArticleProps> = ({ className, article, author, redirectUrl }) => {
   const { title, content, updatedDate, publishedDate, viewCount, labels = [], id } = article;
   const rootClassName = getClassName(classes.article, className);
 
@@ -26,20 +18,22 @@ export const Article: FC<IArticleProps> = ({
             <h2 className={classes.title}>{title}</h2>
             <p className={classes.content}>{content}</p>
           </div>
-          <ArticleImg imgUrl='' className='mt-2' />
+          <ArticleImg imgUrl='' />
         </div>
       </Link>
       <div className={classes.footer}>
         <div>
-          {updatedDate ? (
-            <span>
-              <strong>{toDateString(updatedDate)}</strong> da yangilangan
-            </span>
-          ) : (
-            <span>
-              <strong>{toDateString(publishedDate)}</strong> da nashr etilgan
-            </span>
-          )}
+          <span className='date'>
+            {updatedDate ? (
+              <>
+                <strong>{toDateString(updatedDate)}</strong> da yangilangan
+              </>
+            ) : (
+              <>
+                <strong>{toDateString(publishedDate)}</strong> da nashr etilgan
+              </>
+            )}
+          </span>
           &nbsp; &nbsp;
           {viewCount && (
             <span>
@@ -48,7 +42,7 @@ export const Article: FC<IArticleProps> = ({
           )}
         </div>
         <div>
-          {labels.map((label) => (
+          {labels?.map((label) => (
             <span key={label.id} style={{ marginLeft: '.3rem' }}>
               <Label>{label.name}</Label>
             </span>
@@ -57,7 +51,7 @@ export const Article: FC<IArticleProps> = ({
       </div>
       <div className={classes.footer} style={{ marginTop: '.5rem' }}>
         {author ? <Author {...author} /> : <div />}
-        <div className='d-flex'>
+        {/* <div className='d-flex'>
           {icons &&
             icons.map((icon) => {
               const Icon = ICONS[icon];
@@ -68,7 +62,7 @@ export const Article: FC<IArticleProps> = ({
               );
             })}
           {actions && <Actions actions={actions} />}
-        </div>
+        </div> */}
       </div>
     </div>
   );
