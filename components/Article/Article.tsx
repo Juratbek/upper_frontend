cd9bd7f4aee40744f3aa77440d42f373435948be
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-09-23 14:13:36 +0500

    119:fix/rendering article content as HTML

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -1,6 +1,6 @@
 import { ArticleImg, Author, Label } from 'components';
 import Link from 'next/link';
-import { FC } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import { formatToKMB, getClassName, toDateString } from 'utils';
 
 import classes from './Article.module.scss';
@@ -9,6 +9,18 @@ import { IArticleProps } from './Article.types';
 export const Article: FC<IArticleProps> = ({ className, article, author, redirectUrl }) => {
   const { title, content, updatedDate, publishedDate, viewCount, labels = [], id } = article;
   const rootClassName = getClassName(classes.article, className);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const contentContainer = contentRef.current;
+    const contentMaxChar = 500;
+    if (contentContainer) {
+      const span = document.createElement('span');
+      span.innerHTML =
+        content.length > contentMaxChar ? `${content.slice(0, contentMaxChar)}...` : content;
+      contentContainer.appendChild(span);
+    }
+  }, [content]);
 
   return (
     <div className={rootClassName}>
@@ -16,7 +28,7 @@ export const Article: FC<IArticleProps> = ({ className, article, author, redirec
         <div className={classes.body}>
           <div className={classes['text-content']}>
             <h2 className={classes.title}>{title}</h2>
-            <p className={classes.content}>{content}</p>
+            <p className={classes.content} ref={contentRef} />
           </div>
           <ArticleImg imgUrl='' />
         </div>
