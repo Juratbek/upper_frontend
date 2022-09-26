b4fb329c1261243611f404e90d32a3b6a3fbd84f
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-09-26 16:57:30 +0500

    130:feature/show error if comment message is empty, trimming comment message

--- components/Comments/components/Form/Form.tsx
+++ components/Comments/components/Form/Form.tsx
@@ -9,13 +9,25 @@ import classes from './Form.module.scss';
 import { IFormProps } from './Form.types';
 
 export const Form: FC<IFormProps> = () => {
-  const { register, handleSubmit, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setValue,
+    setError,
+    formState: { errors },
+  } = useForm();
   const [createComment, { isLoading }] = useCreateCommentMutation();
   const {
     query: { id },
   } = useRouter();
 
   const submitHandler = async (event: TSubmitFormEvent): Promise<void> => {
+    const text = event.text.trim();
+    if (!text) {
+      setValue('text', '');
+      setError('text', { message: 'Bo`sh izoh' });
+    }
     if (!id) return Promise.reject();
     await createComment({ text: event.text, articleId: +id });
     reset();
@@ -28,7 +40,10 @@ export const Form: FC<IFormProps> = () => {
         color='transparent'
         {...register('text', { required: true, maxLength: 150 })}
       />
-      <Button loading={isLoading} className='w-100 mx-auto' color='outline-dark'>
+      {errors.text && (
+        <div className='text-start text-danger'>{JSON.stringify(errors.text.message)}</div>
+      )}
+      <Button loading={isLoading} className='w-100 mx-auto mt-1' color='outline-dark'>
         Izoh qoldirish
       </Button>
     </form>
