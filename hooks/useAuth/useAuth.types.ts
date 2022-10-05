c7f8b4df05d4044cbd1d30fa8333e8df126f0cc0
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-10-05 14:47:03 +0500

    162:fix/some type fixings

--- hooks/useAuth/useAuth.types.ts
+++ hooks/useAuth/useAuth.types.ts
@@ -2,7 +2,7 @@ import { TAuthStatus } from 'types';
 
 export interface IUseAuth {
   status: TAuthStatus;
-  isAuthenticated: boolean;
+  isAuthenticated: boolean | undefined;
   authenticate: (token: string) => void;
   unauthenticate: () => void;
   getToken: () => string | null;
