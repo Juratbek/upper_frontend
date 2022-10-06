1914ca5f0eb242dcecd870310559849d1b81fc99
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-10-06 09:45:10 +0500

    165:feature/seperating code into GoogleSignIn component

--- components/GoogleSignIn/GoogleSignIn.tsx
+++ components/GoogleSignIn/GoogleSignIn.tsx
@@ -1,5 +1,30 @@
+import Script from 'next/script';
 import { FC } from 'react';
 
 export const GoogleSignIn: FC = () => {
-  return <div></div>;
+  const onLoadHandler = (): void => {
+    gapi.signin2.render('my-signin2', {
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
+      onsuccess: console.log,
+      onfailure: (err: Record<string, string>) => {
+        console.log('🚀 ~ file: LoginModal.tsx ~ line 85 ~ onLoadHandler ~ err', err);
+      },
+    });
+  };
+
+  return (
+    <>
+      <div id='my-signin2'>sign in</div>
+      <Script
+        src='https://apis.google.com/js/platform.js'
+        async
+        onLoad={onLoadHandler}
+        defer
+      ></Script>
+    </>
+  );
 };
