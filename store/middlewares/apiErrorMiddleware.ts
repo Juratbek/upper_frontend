0a8a13ae0287c62f6f44b85094593fe853a5ca22
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-10-08 13:57:24 +0500

    174:fenhancement/get token by refresh token

--- store/middlewares/apiErrorMiddleware.ts
+++ store/middlewares/apiErrorMiddleware.ts
@@ -1,36 +1,32 @@
 import { AnyAction, isRejectedWithValue, Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
 import { blogApi } from 'store/apis';
 import { IBlogRegisterResponse } from 'store/apis/blog/blog.types';
-import { openLoginModal, unauthenticate } from 'store/states';
+import { authenticate, openLoginModal, unauthenticate } from 'store/states';
 import { REFRESH_TOKEN } from 'variables';
 
-export const apiErrorMiddleware: Middleware = (api: MiddlewareAPI) => (next) => (action) => {
+export const apiErrorMiddleware: Middleware = (api: MiddlewareAPI) => (next) => async (action) => {
   if (isRejectedWithValue(action)) {
     const status: number = action.payload?.status;
     if (status === 401) {
-      const { dispatch, getState } = api;
+      const { dispatch } = api;
       const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+
       if (!refreshToken) {
         dispatch(openLoginModal());
         dispatch(unauthenticate());
         return next(action);
       }
-      const state = getState();
-      //   console.log('🚀 ~ file: apiErrorMiddleware.ts ~ line 19 ~ state', state);
-      const requestUrl = action.meta.baseQueryMeta.request.url as string;
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      const reducerPath = requestUrl.replace(process.env.NEXT_PUBLIC_BASE_URL!, '').split('/')[1];
-      const reducerApi = state[reducerPath];
-      const reducerQueries = reducerApi.queries;
-      const queryCacheKey = action.meta.arg.queryCacheKey;
-      const query = reducerQueries[queryCacheKey];
-      console.log('🚀 ~ file: apiErrorMiddleware.ts ~ line 28 ~ blogApi.reducer', blogApi.reducer);
-      //   console.log('🚀 ~ file: apiErrorMiddleware.ts ~ line 27 ~ query', query);
-      const res = dispatch(
-        blogApi.endpoints.getNewToken.initiate(refreshToken) as unknown as AnyAction,
-      ) as unknown as Promise<{
-        data: IBlogRegisterResponse;
-      }>;
+
+      try {
+        const res = (await dispatch(
+          blogApi.endpoints.getNewToken.initiate(refreshToken) as unknown as AnyAction,
+        )) as unknown as { data: IBlogRegisterResponse };
+        await dispatch(authenticate(res.data));
+        window.location.reload();
+      } catch (e) {
+        console.error(e);
+        return next(action);
+      }
     }
   }
   return next(action);
