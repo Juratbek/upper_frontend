debc6650193fe05ad80bb67f1c3905506e50f39e
Samandar Boymurodov <boymurodovuzcoder@gmail.com>
2022-10-31 22:16:10 +0500

    136: Moved page content to frontends folder

--- pages/write-article/index.tsx
+++ pages/write-article/index.tsx
@@ -1,28 +1,12 @@
-import EditorJS from '@editorjs/editorjs';
-import { Editor } from 'components/Editor';
-import { useBeforeUnload } from 'hooks';
-import { useAppDispatch } from 'store';
-import { setEditor } from 'store/states';
+import { FC } from 'react';
 import { checkAuthInServer } from 'utils';
 
-export default function WriteArticlePage(): JSX.Element {
-  const dispatch = useAppDispatch();
+import { WriteArticlePage } from '../../frontends/write-article';
 
-  useBeforeUnload();
+const WriteArticleNextPage: FC = () => {
+  return <WriteArticlePage />;
+};
 
-  const getInstance = (editor: EditorJS): void => {
-    editor.isReady
-      .then(() => {
-        dispatch(setEditor(editor));
-      })
-      .catch((e) => console.error('err', e));
-  };
-
-  return (
-    <div className='editor-container'>
-      <Editor content={{ blocks: [] }} handleInstance={getInstance} />
-    </div>
-  );
-}
+export default WriteArticleNextPage;
 
 export const getServerSideProps = checkAuthInServer;
