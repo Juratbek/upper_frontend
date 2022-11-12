32f9939fd25e0aa3dfb14fe85ee3a95806cb06c6
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-11-13 00:35:31 +0500

    232:fix/uncommenting window reload

--- hooks/useNextAuth/useNextAuth.ts
+++ hooks/useNextAuth/useNextAuth.ts
@@ -1,15 +1,14 @@
 import { useUrlParams } from 'hooks/useUrlParams/useUrlParams';
+import { nextAuthSignIn, nextAuthSignOut } from 'utils';
 
 import { IUseNextAuthProps } from './useNextAuth.types';
 
 export const useNextAuth = (): IUseNextAuthProps => {
   const { location } = useUrlParams();
 
-  const signIn = (token: string): Promise<Response> => {
-    return fetch(`${location.origin}/api/sign-in?token=${token}`);
-  };
+  const signIn = (token: string): Promise<Response> => nextAuthSignIn(location.origin, token);
 
-  const signOut = (): Promise<Response> => fetch(`${location.origin}/api/sign-out`);
+  const signOut = (): Promise<Response> => nextAuthSignOut(location.origin);
 
   return {
     signIn,
