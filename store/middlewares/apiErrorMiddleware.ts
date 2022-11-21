a094f0378ac4d3d88f6f0e68240f58225d5210ce
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-11-21 13:24:43 +0500

    319:fix/checking api endpoint

--- store/middlewares/apiErrorMiddleware.ts
+++ store/middlewares/apiErrorMiddleware.ts
@@ -1,31 +1,27 @@
 import { AnyAction, isRejectedWithValue, Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
 import { blogApi } from 'store/apis';
 import { IBlogRegisterResponse } from 'store/apis/blog/blog.types';
-import { authenticate, openLoginModal, unauthenticate } from 'store/states';
+import { authenticate, unauthenticate } from 'store/states';
 import { REFRESH_TOKEN } from 'variables';
 
 export const apiErrorMiddleware: Middleware = (api: MiddlewareAPI) => (next) => async (action) => {
   if (isRejectedWithValue(action)) {
     const status: number = action.payload?.status;
+
     if (status === 401) {
       const { dispatch } = api;
       const refreshToken = localStorage.getItem(REFRESH_TOKEN);
+      const endpointName = action.meta?.arg?.endpointName;
+      const isGetNewTokenApi = endpointName === 'getNewToken';
 
-      if (!refreshToken) {
-        dispatch(openLoginModal());
+      if (!refreshToken || isGetNewTokenApi) {
         dispatch(unauthenticate());
-        return next(action);
-      }
-
-      try {
+      } else {
         const res = (await dispatch(
           blogApi.endpoints.getNewToken.initiate(refreshToken) as unknown as AnyAction,
         )) as unknown as { data: IBlogRegisterResponse };
         await dispatch(authenticate(res.data));
         window.location.reload();
-      } catch (e) {
-        console.error(e);
-        return next(action);
       }
     }
   }
