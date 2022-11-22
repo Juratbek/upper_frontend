3c6c269cf5f6beb9f6c1d82f26e277badea5f5b2
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-11-22 14:08:19 +0500

    267:fix/Hiding sidebar on mobile UI

--- hooks/useDevice/useDevice.ts
+++ hooks/useDevice/useDevice.ts
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import { getDevice, IDevice } from 'utils';
 
-export const useDevice = (): IDevice => {
+export const useDevice = (defaultDevice?: Partial<IDevice>): IDevice => {
   const [device, setDevice] = useState<IDevice>({
     isMobile: false,
     isTablet: false,
     type: 'unknown',
+    ...defaultDevice,
   });
 
   useEffect(() => {
