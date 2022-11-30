d9aae7d26fedcf0ddc6446b4e59871563e083717
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-11-30 20:54:22 +0500

    337:fix/rewrting lost changes

---
+++ pages/create-label/index.tsx
@@ -0,0 +1,11 @@
+import { CreateLabelForm } from 'frontends/create-label';
+
+const CreateLabelPage = (): JSX.Element => {
+  return (
+    <div className='container'>
+      <CreateLabelForm />
+    </div>
+  );
+};
+
+export default CreateLabelPage;
