b9cfa30a51b9e4a085d9379747157b29dd00b8ef
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-12-04 21:23:02 +0500

    401:feature/implementing contacts page

---
+++ frontends/contacts/contacts/Contacts.constants.ts
@@ -0,0 +1,16 @@
+import { ICONS } from 'variables';
+
+import { IContact } from './Contacts.types';
+
+export const CONTACTS: IContact[] = [
+  {
+    icon: ICONS.telegram,
+    link: 'https://t.me/upper_contact_bot',
+    text: 'Telegram',
+  },
+  {
+    icon: ICONS.instagram,
+    link: 'https://www.instagram.com/upperuz/',
+    text: 'Instagram',
+  },
+];
