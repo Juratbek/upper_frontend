b299954aefe5d1028b26b5b47e06e086219c80b4
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-12-24 16:01:23 +0500

    config/configuring for new dev environment

--- next.config.js
+++ next.config.js
@@ -3,13 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: [
-      'www.lamborghini.com',
       'images.unsplash.com',
       'lh3.googleusercontent.com',
-      'i.natgeofe.com',
       't.me',
-      'upper-article-image-assets-bucket.s3.ap-south-1.amazonaws.com',
-      'upper-blog-image-assets-bucket.s3.ap-south-1.amazonaws.com'
+      'upper-dev-article-img-bucket.s3.ap-south-1.amazonaws.com',
+      'upper-dev-blog-img-bucket.s3.ap-south-1.amazonaws.com',
     ],
   },
 };
