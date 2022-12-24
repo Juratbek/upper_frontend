0664a224a5ef291a65101c71be4a5d90a8707341
makhajur <macbookpro@macbooks-MacBook-Pro-2.local>
2022-12-24 16:07:45 +0500

    459:fix/type fixes

--- store/apis/config/api.constants.ts
+++ store/apis/config/api.constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-export const ARTICLE_BUCKET_URL = process.env.NEXT_PUBLIC_ARTICLE_BUCKET_URL;
+export const ARTICLE_BUCKET_URL = process.env.NEXT_PUBLIC_ARTICLE_BUCKET_URL || '';
 
-export const BLOG_BUCKET_URL = process.env.NEXT_PUBLIC_BLOG_BUCKET_URL;
+export const BLOG_BUCKET_URL = process.env.NEXT_PUBLIC_BLOG_BUCKET_URL || '';
 
 export const UNSPLASH_URL = 'https://images.unsplash.com/';
