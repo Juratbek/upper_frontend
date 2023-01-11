aeb97fa715b02126101a7d0d1ebc3ca61042e53d
@musojontursunov1 <musojontursunov1@gmail.com>
2023-01-11 15:00:26 +0500

    562:code quality/added feature code qualited

--- components/Comments/components/Comment/Comment.tsx
+++ components/Comments/components/Comment/Comment.tsx
@@ -1,6 +1,6 @@
 import { Avatar } from 'components';
 import { Divider } from 'components/Divider/Divider';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { FC } from 'react';
 import { toDateString } from 'utils';
 
@@ -8,19 +8,15 @@ import classes from './Comment.module.scss';
 import { TCommentProps } from './Comment.types';
 
 export const Comment: FC<TCommentProps> = ({ author, date, text }) => {
-  const router = useRouter();
-  const clickHandler = (): void => {
-    router.push(`/blogs/${author.id}`);
-  };
   return (
     <>
       <div className={classes.comment}>
         <div className={classes.author}>
           <Avatar imgUrl={author.imgUrl} size='small' />
           <div className='ms-1'>
-            <h5 onClick={clickHandler} className='m-0 link pointer'>
-              {author.name}
-            </h5>
+            <Link href={`/blogs/${author.id}`}>
+              <h5 className='m-0 link pointer'>{author.name}</h5>
+            </Link>
             <p className={`m-0 ${classes.date}`}>{toDateString(date)}</p>
           </div>
         </div>
