359f968d4d6c2fe488a670f43953d837640cc89a
@musojontursunov1 <musojontursunov1@gmail.com>
2023-01-20 15:22:38 +0500

    631:enhancemend/delete the neglected unused const

--- frontends/home/Home.constants.ts
+++ frontends/home/Home.constants.ts
@@ -1,8 +1,3 @@
-export const HOME_TAB_IDS = {
-  forYou: 'forYou',
-  top: 'top',
-};
-
 export const ForYouLabel = { name: 'Siz uchun', id: 'for-you' };
 
 export const TopLabel = { name: 'Top', id: 'top' };
