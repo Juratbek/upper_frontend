1a7618085a3b362fcd18c9cd2968cea4b3a417e1
@musojontursunov1 <musojontursunov1@gmail.com>
2023-01-20 10:03:02 +0500

    631:enhancement/delete unesed Home const and forYou and topTab components

--- frontends/home/Home.constants.ts
+++ frontends/home/Home.constants.ts
@@ -1,30 +1,6 @@
-import { ITabBody, ITabHeader } from 'types';
-
-import { ForYouTab } from './ForYouTab/ForYouTab';
-import { TopTab } from './TopTab/TopTab';
-
 export const HOME_TAB_IDS = {
   forYou: 'forYou',
   top: 'top',
-};
-
-export const HOME_TAB_MENUS: ITabHeader[] = [
-  {
-    name: 'Siz uchun',
-    id: HOME_TAB_IDS.forYou,
-    private: true,
-    defaultSelected: true,
-  },
-  {
-    name: 'Top',
-    id: HOME_TAB_IDS.top,
-    defaultSelected: false,
-  },
-];
-
-export const HOME_TABS: ITabBody = {
-  [HOME_TAB_IDS.forYou]: ForYouTab,
-  [HOME_TAB_IDS.top]: TopTab,
 };
 
 export const ForYouLabel = { name: 'Siz uchun', id: 'for-you' };
