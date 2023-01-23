6550c760f7bee9298ec0cbca3f77b6fb93b698cd
makhajur <macbookpro@Juratbek-Mac.local>
2023-01-23 23:37:16 +0500

    644:enhancement/moving some components into lib

--- components/WithPopover/WithPopover.types.ts
+++ components/WithPopover/WithPopover.types.ts
@@ -1,4 +1,4 @@
-import { IButtonProps } from 'components/Button/Button.types';
+import { IButtonProps } from 'components';
 import { ReactNode } from 'react';
 
 export interface IWithPopoverProps extends IButtonProps {
