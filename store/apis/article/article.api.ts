4c058d0cc27e9a3a44b08be26b686da759bd1ac9
makhajur <macbookpro@Juratbek-Mac.local>
2023-01-25 11:02:42 +0500

    661:enhancement/showing status and statistics in article item

--- store/apis/article/article.api.ts
+++ store/apis/article/article.api.ts
@@ -1,11 +1,5 @@
 import { createApi } from '@reduxjs/toolkit/dist/query/react';
-import {
-  IArticle,
-  IArticleResult,
-  IPagingResponse,
-  TArticleStatus,
-  TOptionalPagingRequest,
-} from 'types';
+import { IArticle, IArticleResult, IPagingResponse, TOptionalPagingRequest } from 'types';
 import { PAGINATION_SIZE } from 'variables';
 
 import { baseQuery } from '../config';
@@ -20,12 +14,8 @@ export const articleApi = createApi({
     getBlogArticleById: build.query<IArticle, number>({
       query: (id: number) => `need-auth/${id}`,
     }),
-    getBlogArticles: build.query<
-      IPagingResponse<IArticleResult>,
-      TOptionalPagingRequest<{ statuses: TArticleStatus[] }>
-    >({
-      query: ({ statuses, page = 0 }) =>
-        `need-auth/list?statuses=${statuses}&page=${page}&size=${PAGINATION_SIZE}`,
+    getBlogArticles: build.query<IPagingResponse<IArticleResult>, TOptionalPagingRequest>({
+      query: ({ page = 0 }) => `need-auth/list?page=${page}&size=${PAGINATION_SIZE}`,
     }),
     publish: build.mutation<IArticle, number>({
       query: (id) => ({
