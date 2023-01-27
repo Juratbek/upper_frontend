eccd602d300b669c7448f4eec22d98b94fe4c493
@musojontursunov1 <musojontursunov1@gmail.com>
2023-01-27 21:57:08 +0500

    669:bug/adding translation text in articel status

--- components/lib/Status/Status.tsx
+++ components/lib/Status/Status.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
+import { TArticleStatus } from 'types';
 import { getClassName } from 'utils';
 
 import classes from './Status.module.scss';
-import { IStatusProps } from './Status.types';
+import { IStatusProps, STATUS_LABELS } from './Status.types';
 
 export const Status: FC<IStatusProps> = ({ status, className, children }) => {
   const rootClassName = getClassName(classes.root, classes[status.toLowerCase()], className);
 
-  return <span className={rootClassName}>{children}</span>;
+  return <span className={rootClassName}>{STATUS_LABELS[children as TArticleStatus]}</span>;
 };
--- components/lib/Status/Status.types.ts
+++ components/lib/Status/Status.types.ts
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { TArticleStatus } from 'types';
 
 type TStatus = 'PUBLISHED' | 'SAVED';
 
@@ -7,3 +8,8 @@ export interface IStatusProps {
   status: TStatus;
   children?: ReactNode;
 }
+
+export const STATUS_LABELS: Record<TArticleStatus, string> = {
+  PUBLISHED: 'Nashr qilingan',
+  SAVED: 'Saqlangan',
+};
