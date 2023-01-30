f9219e86f52a160246e094f65231322d70173f0c
@musojontursunov1 <musojontursunov1@gmail.com>
2023-01-30 14:16:18 +0500

    677:enhancement/I delete code Which I added in this branch because I was not able to do this task

--- components/lib/Head/Head.tsx
+++ components/lib/Head/Head.tsx
@@ -5,7 +5,7 @@ import { IHeadProps } from './Head.types';
 
 export const Head: FC<IHeadProps> = (props) => {
   const { title, imgUrl, type, url, author, publishedDate } = props;
-  console.log(props);
+
   const description = useMemo(() => {
     const { description } = props;
     if (!description) return '';
@@ -17,15 +17,13 @@ export const Head: FC<IHeadProps> = (props) => {
       return '';
     }
   }, [props.description]);
-  console.log('🚀 ~ file: Head.tsx:10 ~ description ~ description', description);
 
   return (
     <NextHead>
       <meta property='og:site_name' content='UPPER' />
-      <meta property='og:title' content={`${author} Upper da:${title}`} key='og-title' />
+      <meta property='og:title' content={title} key='og-title' />
       <meta property='og:image' content={imgUrl || '/social_medi_logo.png'} />
-      <meta property='og:description' content={description?.slice(0, 200) || ''} />
-      <meta name='viewport' content='width=device-width, initial-scale=20' />
+      <meta property='og:description' content={description || ''} />
       <meta property='og:type' content={type || ''} />
       <meta property='og:locale' content='uz' />
       <meta property='og:url' content={url} />
