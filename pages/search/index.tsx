5fbc2e9e0e41e68f4e3554c0b89a7e9916ef3814
makhajur <macbookpro@Juratbek-Mac.local>
2023-02-01 16:14:28 +0500

    699:enhancement/getting search input value from params

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,21 +1,34 @@
 import { Head, SearchInput, TabBody, TabsHeader } from 'components';
 import { useUrlParams } from 'hooks';
 import type { NextPage } from 'next';
+import { useMemo } from 'react';
 import classes from 'styles/Search.module.scss';
 import { SEARCH_TAB_MENUS, SEARCH_TABS } from 'variables/Search.constants';
 
 const SearchPage: NextPage = () => {
-  const { setParam } = useUrlParams();
+  const { setParam, getParam, isReady } = useUrlParams();
 
   const search = (value: string): void => {
     value && setParam('search', value);
   };
 
+  const searchInput = useMemo(() => {
+    if (isReady) {
+      return (
+        <SearchInput
+          defaultValue={getParam('search')}
+          className={`${classes['search-input']} my-2`}
+          placeholder='Qidirish uchun yozing'
+          onDebounce={search}
+        />
+      );
+    }
+  }, [isReady]);
+
   return (
     <div className='container'>
       <Head title='Qidirish' url='/search' />
-      <h1 className='mb-1'>Qidirish</h1>
-      <SearchInput className={`${classes['search-input']} mb-2`} onDebounce={search} />
+      {searchInput}
       <TabsHeader tabs={SEARCH_TAB_MENUS} />
       <TabBody tabs={SEARCH_TABS} />
     </div>
