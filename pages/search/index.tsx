50a7a41c979f5ab5736b24524fa05470a0d115bb
makhajur <macbookpro@Juratbek-Mac.local>
2023-02-01 21:36:26 +0500

    701:fix/revering search input default value change

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,34 +1,24 @@
 import { Head, SearchInput, TabBody, TabsHeader } from 'components';
 import { useUrlParams } from 'hooks';
 import type { NextPage } from 'next';
-import { useMemo } from 'react';
 import classes from 'styles/Search.module.scss';
 import { SEARCH_TAB_MENUS, SEARCH_TABS } from 'variables/Search.constants';
 
 const SearchPage: NextPage = () => {
-  const { setParam, getParam, isReady } = useUrlParams();
+  const { setParam } = useUrlParams();
 
   const search = (value: string): void => {
     value && setParam('search', value);
   };
 
-  const searchInput = useMemo(() => {
-    if (isReady) {
-      return (
-        <SearchInput
-          defaultValue={getParam('search')}
-          className={`${classes['search-input']} my-2`}
-          placeholder='Qidirish uchun yozing'
-          onDebounce={search}
-        />
-      );
-    }
-  }, [isReady]);
-
   return (
     <div className='container'>
       <Head title='Qidirish' url='/search' />
-      {searchInput}
+      <SearchInput
+        className={`${classes['search-input']} my-2`}
+        placeholder='Qidirish uchun yozing'
+        onDebounce={search}
+      />
       <TabsHeader tabs={SEARCH_TAB_MENUS} />
       <TabBody tabs={SEARCH_TABS} />
     </div>
