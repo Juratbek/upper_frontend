076203d4ba9bca13923a8f1c670b205738124d7b
@musojontursunov1 <musojontursunov1@gmail.com>
2023-02-08 10:39:55 +0500

    707:enhancement/remove sizes

--- components/skeletons/NotificationSkeleton/NotificationSkeleton.tsx
+++ components/skeletons/NotificationSkeleton/NotificationSkeleton.tsx
@@ -3,18 +3,13 @@ import { FC } from 'react';
 import classes from './NotificationSkeleton.module.scss';
 import { INotificationSkeletonProps } from './NotificationSkeleton.type';
 
-export const NotificationSkeleton: FC<INotificationSkeletonProps> = ({
-  size = 'medium',
-  ...props
-}) => {
-  const sizeClassName = classes[`author--${size}`];
-  const textSizeClassName = classes[`text--${size}`];
+export const NotificationSkeleton: FC<INotificationSkeletonProps> = (props) => {
   return (
     <div className={`d-flex align-items-center ${props.className}`}>
-      <div className={`${classes.author} skeleton ${sizeClassName}`} />
+      <div className={`${classes.author} skeleton`} />
       <div className='flex-1'>
-        <p className={`${classes.text} ${textSizeClassName} w-100 skeleton`} />
-        <p className={`${classes.text} ${textSizeClassName} w-30 skeleton`} />
+        <p className={`${classes.text} w-100 skeleton`} />
+        <p className={`${classes.text} w-30 skeleton`} />
       </div>
     </div>
   );
