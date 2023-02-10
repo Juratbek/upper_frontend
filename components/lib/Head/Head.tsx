5c401c1fd43eba6117695a49ae5eaeee6c4002ec
makhajur <macbookpro@Juratbek-Mac.local>
2023-02-10 17:39:31 +0500

    677:fix/description meta tag

--- components/lib/Head/Head.tsx
+++ components/lib/Head/Head.tsx
@@ -4,6 +4,7 @@ import { FC, useMemo } from 'react';
 import { IHeadProps } from './Head.types';
 
 export const Head: FC<IHeadProps> = (props) => {
+  console.log('🚀 ~ file: Head.tsx:7 ~ props', props);
   const { title, imgUrl, type, url, author, publishedDate } = props;
 
   const description = useMemo(() => {
@@ -14,10 +15,11 @@ export const Head: FC<IHeadProps> = (props) => {
       div.innerHTML = description;
       return div.textContent;
     } catch (e) {
+      console.error(e);
       return '';
     }
   }, [props.description]);
-  console.log({ description });
+
   return (
     <NextHead>
       <meta property='og:site_name' content='UPPER' />
@@ -28,7 +30,7 @@ export const Head: FC<IHeadProps> = (props) => {
       <meta property='og:image:width' content='2048' />
       <meta property='og:image:height' content='2048' />
       <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <meta name='description' content={description || 'Bo`sh'} key='description' />
+      <meta name='description' content={props.description || 'Bo`sh'} key='description' />
       <meta property='og:article:author' content={author || 'Author Name'} />
       <meta property='og:locale' content='uz' />
       <meta property='og:url' content={url} />
