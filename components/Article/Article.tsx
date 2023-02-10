502638b0f2379113e8ea650118be7c1e3c6bce0d
makhajur <macbookpro@Juratbek-Mac.local>
2023-02-10 14:26:41 +0500

    717:enhancement/clean code

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -55,10 +55,7 @@ export const Article: FC<IArticleProps> = ({ article, author, redirectUrl, ...pr
             </div>
             {imgUrl && <ArticleImg imgUrl={imgUrl} />}
           </div>
-          <div
-            className={classes.footer}
-            style={{ marginTop: props.showStatus && status ? '0.5rem' : '0.3rem' }}
-          >
+          <div className={classes.footer} style={{ marginTop: props.showStatus ? '0.5rem' : 0 }}>
             <div className={classes.stats}>
               <time>
                 <span className={classes.icon}>
