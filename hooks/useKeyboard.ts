4b34a2379e0be92ca06e016f248b6929ff347335
@musojontursunov1 <musojontursunov1@gmail.com>
2023-02-11 17:42:39 +0500

    606:enhancement/ctrlKey in useKeyboard is turned to dynamic

--- hooks/useKeyboard.ts
+++ hooks/useKeyboard.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 import { addKeyboardListeners } from 'utils';
 
-export const useKeyboard = (targetKey: string): boolean => {
+export const useKeyboard = (targetKey: string, ctrlKey = true): boolean => {
   const [isKeyPressed, setIsKeyPressed] = useState(false);
 
   useEffect(() => {
     const listener = addKeyboardListeners(
       [
-        { key: targetKey, ctrlKey: true },
-        { key: targetKey, metaKey: true },
+        { key: targetKey, ctrlKey },
+        { key: targetKey, metaKey: ctrlKey },
       ],
       (event) => {
         event?.preventDefault();
