979d9bd31ad5febfbb8ac6ebe286ffd6a6e89b11
makhajur <macbookpro@Juratbek-Mac.local>
2023-02-11 09:58:35 +0500

    733:enhancement/adding initial search UI

--- frontends/search/SearchPage.tsx
+++ frontends/search/SearchPage.tsx
@@ -1,16 +1,41 @@
-import { Head, SearchInput, TabBody, TabsHeader } from 'components';
+import { Head, SearchInput, StorysetImage, TabBody, TabsHeader } from 'components';
 import { useUrlParams } from 'hooks';
 import { FC } from 'react';
 
 import { SEARCH_TAB_MENUS, SEARCH_TABS } from './Search.constants';
 import classes from './Search.module.scss';
 
+const searchParamKey = 'search';
+
 export const SearchPage: FC = () => {
-  const { setParam } = useUrlParams();
+  const { setParam, getParam, isReady } = useUrlParams();
+  const searchValue = getParam(searchParamKey);
 
   const search = (value: string): void => {
-    value && setParam('search', value);
+    value && setParam(searchParamKey, value);
   };
+
+  if (!isReady) return <></>;
+
+  if (!searchValue) {
+    return (
+      <div className='d-flex flex-1 align-items-center justify-content-center flex-col'>
+        <StorysetImage
+          storysetUri='data'
+          width={300}
+          height={300}
+          className='mb-2'
+          src='/storyset/search_data.svg'
+        />
+        <SearchInput
+          className={classes['search-input']}
+          placeholder='Qidirish uchun yozing'
+          onDebounce={search}
+        />
+        <div style={{ height: '5rem' }} />
+      </div>
+    );
+  }
 
   return (
     <div className='container'>
@@ -19,6 +44,7 @@ export const SearchPage: FC = () => {
         className={`${classes['search-input']} my-2`}
         placeholder='Qidirish uchun yozing'
         onDebounce={search}
+        defaultValue={searchValue}
       />
       <TabsHeader tabs={SEARCH_TAB_MENUS} />
       <TabBody tabs={SEARCH_TABS} />
