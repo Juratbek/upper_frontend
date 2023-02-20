fb1bc9097986d6cc9eb30616ba589ee72370b5ee
Ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-02-20 10:53:53 +0500

    #815: fix/changed the color of the icons

--- frontends/contacts/contacts/Contacts.tsx
+++ frontends/contacts/contacts/Contacts.tsx
@@ -1,16 +1,18 @@
+import { useTheme } from 'hooks';
 import Link from 'next/link';
 
 import { CONTACTS } from './Contacts.constants';
 import classes from './Contacts.module.scss';
 
 export const Contacts = (): JSX.Element => {
+  const { theme } = useTheme();
   return (
     <div className={`${classes.contacts} flex-mobile-col`}>
       {CONTACTS.map((contact) => (
         <Link href={contact.link} key={contact.link}>
           <a target='_blank' className={`${classes.contact} mb-4 card w-30 w-mobile-100`}>
             <span className={classes.icon}>
-              <contact.icon />
+              <contact.icon color={theme === 'dark' ? 'white' : 'black'} />
             </span>
             <h3>{contact.text}</h3>
           </a>
--- frontends/team/TeamPage.tsx
+++ frontends/team/TeamPage.tsx
@@ -1,10 +1,12 @@
 import { Avatar } from 'components';
+import { useTheme } from 'hooks';
 import Link from 'next/link';
 
 import { team } from './TeamPage.constants';
 import classes from './TeamPage.module.scss';
 
 export const TeamPage = (): JSX.Element => {
+  const { theme } = useTheme();
   return (
     <div className='container pt-3'>
       <h1 className='text-center'>Bizning Jamoa</h1>
@@ -21,7 +23,7 @@ export const TeamPage = (): JSX.Element => {
               {member.links.map((link) => (
                 <Link href={link.url} key={link.url} target={link.target || '_blank'}>
                   <a className={classes.icon}>
-                    <link.icon />
+                    <link.icon color={theme === 'dark' ? 'white' : 'black'} />
                   </a>
                 </Link>
               ))}
