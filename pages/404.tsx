33dcbf7dfa7a4d031974ed38f500fd29705102b0
makhajur <macbookpro@Juratbek-Mac-2.local>
2023-02-23 10:51:11 +0500

    860:fix/syncing auth with server in 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,20 @@
 import { Head } from 'components';
 import { NotFound } from 'frontends/404';
+import { useAuth } from 'hooks';
 import { NextPage } from 'next';
+import { useEffect } from 'react';
 
 const NotFoundPage: NextPage = () => {
+  const { getToken, getRefreshToken, authenticate } = useAuth();
+
+  useEffect(() => {
+    const token = getToken();
+    const refreshToken = getRefreshToken() || '';
+    if (token) {
+      authenticate({ token, refreshToken });
+    }
+  }, []);
+
   return (
     <div>
       <Head title='Sahifa topilmadi' url='/not-fount' />
