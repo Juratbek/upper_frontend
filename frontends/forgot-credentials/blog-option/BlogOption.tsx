67eaa630ccb6d0832cfe00074f961f9e1bf2b6cc
makhajur <macbookpro@Juratbek-Mac-2.local>
2023-02-28 18:42:21 +0500

    885:fix/refactoring forgot credentials form for multiple user select

---
+++ frontends/forgot-credentials/blog-option/BlogOption.tsx
@@ -0,0 +1,14 @@
+import { Avatar } from 'components';
+import { FC } from 'react';
+import { IBlogSmall } from 'types';
+
+export const BlogOption: FC<IBlogSmall> = (blog) => {
+  return (
+    <div className='d-flex'>
+      <Avatar size='medium' imgUrl={blog.imgUrl} />
+      <div className='ms-1 d-flex align-items-center'>
+        <h4 className='m-0'>{blog.name}</h4>
+      </div>
+    </div>
+  );
+};
