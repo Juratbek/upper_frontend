718b8437501e02d729c20a4b8baed9a7a28ce618
Ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-03-02 12:31:12 +0500

    488: feature/github gist has integrated to editorjs

--- components/Editor/services/editor-tools.ts
+++ components/Editor/services/editor-tools.ts
@@ -132,6 +132,13 @@ export const getTools = async (): Promise<EditorConfig['tools']> => {
             height: 500,
             width: 600,
           },
+          github: {
+            regex: /https?:\/\/gist.github.com\/([^\/\?\&]*)\/([^\/\?\&]*)/,
+            embedUrl:
+              'data:text/html;charset=utf-8,<head><base target="_blank" /></head><body><script src="https://gist.github.com/<%= remote_id %>" ></script></body>',
+            html: '<iframe width="100%" frameborder="no" height="550" src=""></iframe>',
+            id: (groups: any) => `${groups.join('/')}.js`,
+          },
         },
       },
     },
