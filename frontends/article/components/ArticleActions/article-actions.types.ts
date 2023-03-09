e7c688c1486a83ffb486e149ef3bbd5c3defc8be
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-03-09 08:20:00 +0500

    946:fix/adding -1 to like count on dislike if article was liked before

--- frontends/article/components/ArticleActions/article-actions.types.ts
+++ frontends/article/components/ArticleActions/article-actions.types.ts
@@ -2,16 +2,21 @@ import EditorJS from '@editorjs/editorjs';
 import { Dispatch, SetStateAction } from 'react';
 import { IArticle } from 'types';
 
-type TLikeDislikeFn = () => void;
+type TLikeFn = () => void;
+
+type TDislikeFn = (wasLikedBefore: boolean) => void;
+
 export interface IArticleActionsProps {
   editor: EditorJS | null;
   article: IArticle | null;
-  onLike: TLikeDislikeFn;
+  onLike: TLikeFn;
+  onDislike: TDislikeFn;
 }
 
 export interface IArticleActionsIconsProps {
   article: IArticle | null;
-  onLike: TLikeDislikeFn;
+  onLike: TLikeFn;
+  onDislike: TDislikeFn;
   isSharePopupOpen: boolean;
   setIsSharePopupOpen: Dispatch<SetStateAction<boolean>>;
   popupId: string;
