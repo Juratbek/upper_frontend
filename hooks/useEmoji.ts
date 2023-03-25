96999024a0515cef8e1679e6ea966295eed8e3e9
Samandar Boymurodov <boymurodovuzcoder@gmail.com>
2023-03-26 02:25:41 +0500

    find and replace the range with a hardcoded string

--- hooks/useEmoji.ts
+++ hooks/useEmoji.ts
@@ -1,5 +1,38 @@
 import EditorJs from '@editorjs/editorjs';
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+
+function getCaretCoordinates(element: HTMLElement, position: number): DOMRect {
+  const range = document.createRange();
+
+  let totalOffset = 0;
+  let textNode = null;
+
+  // Find the text node that contains the position
+  const findTextNode = function (node: Node): void {
+    if (node.nodeType === Node.TEXT_NODE) {
+      const len = (node.textContent as string).length;
+      if (totalOffset <= position && totalOffset + len >= position) {
+        textNode = node;
+      }
+      totalOffset += len;
+    } else {
+      for (let i = 0, len = node.childNodes.length; i < len; ++i) {
+        findTextNode(node.childNodes[i]);
+      }
+    }
+  };
+
+  findTextNode(element);
+
+  // Set the range to the caret position
+  // @ts-ignore
+  range.setStart(textNode, position - (totalOffset - textNode.textContent.length));
+  // @ts-ignore
+  range.setEnd(textNode, position - (totalOffset - textNode.textContent.length));
+
+  // Get the bounding rectangle of the range
+  return range.getBoundingClientRect();
+}
 
 function getCaretCharacterOffsetWithin(element: HTMLElement): number {
   let caretOffset = 0;
@@ -13,12 +46,62 @@ function getCaretCharacterOffsetWithin(element: HTMLElement): number {
   }
   return caretOffset;
 }
+
+function replaceRange(start: number, end: number, el: HTMLElement): void {
+  const range = document.createRange();
+  const sel = window.getSelection() as Selection;
+
+  let totalOffset = 0;
+  let startTextNode: Node | null = null;
+  let endTextNode: Node | null = null;
+
+  const findTextNodes = function (node: Node): void {
+    if (node.nodeType === Node.TEXT_NODE) {
+      const len = (node.textContent as string).length;
+      if (totalOffset <= start && totalOffset + len >= start) {
+        startTextNode = node;
+      }
+      if (totalOffset <= end && totalOffset + len >= end) {
+        endTextNode = node;
+      }
+      totalOffset += len;
+    } else {
+      for (let i = 0, len = node.childNodes.length; i < len; ++i) {
+        findTextNodes(node.childNodes[i]);
+      }
+    }
+  };
+
+  findTextNodes(el);
+
+  // Extract the range from the text nodes
+  // @ts-ignore
+  range.setStart(startTextNode, start - (totalOffset - startTextNode.textContent.length));
+  // @ts-ignore
+  range.setEnd(endTextNode, end - totalOffset + endTextNode.textContent.length);
+
+  // Create and insert the new element
+  const newElement = document.createTextNode('😀');
+  range.deleteContents();
+  range.insertNode(newElement);
+  range.setStartAfter(newElement);
+  range.setEndAfter(newElement);
+  // Reset the selection to include the new element
+  // range.selectNodeContents(newElement);
+  sel.removeAllRanges();
+  sel.addRange(range);
+}
 export const useEmoji = (editor: null | EditorJs): void => {
+  const positionsList = useRef<number[]>([]);
+  const caretCoords = useRef<DOMRect | null>(null);
+
   const handleKeyPress = useCallback(
     (e: InputEvent): void => {
       const target = e.target as HTMLDivElement;
-      console.log(getCaretCharacterOffsetWithin(target));
       if (e.data === ':') {
+        const caretPosition = getCaretCharacterOffsetWithin(target);
+        positionsList.current = [caretPosition];
+        caretCoords.current = getCaretCoordinates(target, caretPosition);
         // @ts-ignore
         target.addEventListener('input', queryListener);
       }
@@ -27,8 +110,26 @@ export const useEmoji = (editor: null | EditorJs): void => {
   );
 
   const queryListener = useCallback((e: InputEvent): void => {
-    debugger;
-    console.log('I am tracking search query');
+    const target = e.target as HTMLDivElement;
+    const currentCaretPosition = getCaretCharacterOffsetWithin(target);
+    const lastPosition = positionsList.current[positionsList.current.length - 1];
+    if (
+      (currentCaretPosition >= lastPosition || lastPosition - 1 === currentCaretPosition) &&
+      positionsList.current[0] - 1 !== currentCaretPosition
+    ) {
+      positionsList.current.push(currentCaretPosition);
+      if (e.data === 'z') {
+        replaceRange(positionsList.current[0] - 1, getCaretCharacterOffsetWithin(target), target);
+        // @ts-ignore
+        target.removeEventListener('input', queryListener);
+      }
+    } else {
+      // @ts-ignore
+      target.removeEventListener('input', queryListener);
+    }
+    console.log(positionsList.current);
+    console.log(currentCaretPosition);
+    console.log(caretCoords.current);
   }, []);
 
   useEffect(() => {
