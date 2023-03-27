67f2514e658128f3becd0b95f84a00435c1c11e2
Samandar Boymurodov <boymurodovuzcoder@gmail.com>
2023-03-28 00:55:36 +0500

    876: Added popover to choose emoji from

--- components/EmojiPopover/EmojiPopover.tsx
+++ components/EmojiPopover/EmojiPopover.tsx
@@ -1,8 +1,69 @@
-import { FC } from 'react';
+import { FC, useLayoutEffect, useMemo, useRef } from 'react';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface IEmojiPopoverProps {}
+import { emojis } from './emoji';
+import styles from './EmojiPopover.module.scss';
 
-export const EmojiPopover: FC<IEmojiPopoverProps> = () => {
-  return <div>EmojiPopover</div>;
+interface IEmojiPopoverProps {
+  emojiQuery: string;
+  onEmojiClick: (emoji: string) => void;
+  targetTextCoords: DOMRect;
+  targetTextContainer: HTMLElement;
+}
+
+export const EmojiPopover: FC<IEmojiPopoverProps> = ({
+  emojiQuery,
+  onEmojiClick,
+  targetTextCoords,
+  targetTextContainer,
+}) => {
+  const popoverEl = useRef<HTMLDivElement>(null);
+  const emojiListKeys = useMemo<string[]>(() => {
+    return Object.keys(emojis);
+  }, []);
+
+  useLayoutEffect(() => {
+    if (popoverEl.current) {
+      const editorContainer = document.querySelector('.editor-container') as HTMLElement;
+      const editorContainerRect = editorContainer.getBoundingClientRect();
+      const top =
+        targetTextCoords.top - editorContainerRect.top + targetTextContainer.offsetHeight / 2 + 10;
+      const left = targetTextCoords.left - editorContainerRect.left;
+      // set the popover modal position
+      popoverEl.current.style.top = top + 'px';
+      popoverEl.current.style.left = left + 'px';
+    }
+  }, []);
+
+  const matchedEmojis = useMemo(() => {
+    const queryKey = emojiQuery.split(/\s+/).join('_');
+
+    const matchedEmojiKeys = emojiListKeys.filter((emojiKey) => emojiKey.includes(queryKey));
+
+    return matchedEmojiKeys.map((emojiKey) => ({
+      key: emojiKey,
+      emoji: emojis[emojiKey],
+    }));
+  }, [emojiQuery]);
+
+  return (
+    <div className={styles.emojiContainer} ref={popoverEl}>
+      {matchedEmojis.map((em, i) => (
+        <span
+          key={i}
+          className={styles.emojiItem}
+          title={em.key}
+          onClick={(): void => onEmojiClick(em.emoji)}
+          ref={
+            i === 0
+              ? (e): void => {
+                  e?.focus();
+                }
+              : undefined
+          }
+        >
+          {em.emoji}
+        </span>
+      ))}
+    </div>
+  );
 };
