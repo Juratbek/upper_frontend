d4927223de536f8126702398a62ca225a9c5606d
ayyubxon-coder <ayyubxonkamoldinovcoder@gmail.com>
2023-04-18 23:33:20 +0500

    #1002/feature Ayyubxon and Musojon has been added and UI bug fixed

--- frontends/team/TeamPage.constants.ts
+++ frontends/team/TeamPage.constants.ts
@@ -5,12 +5,13 @@ import { ITeamMember } from './TeamPage.types';
 export const team: ITeamMember[] = [
   {
     name: "Jur'atbek",
-    imgUrl: '/team/jurat.jpeg',
+    imgUrl: '/team/juratbek.jpg',
     position: 'Software Development Engineer',
     links: [
       {
         url: 'https://www.linkedin.com/in/jur-atbek-mahammadaliyev-b66396203',
         icon: ICONS.linkedIn,
+        target: '_self',
       },
       {
         url: '/blogs/1',
@@ -27,9 +28,43 @@ export const team: ITeamMember[] = [
       {
         url: 'https://www.linkedin.com/in/boymurodov-samandar',
         icon: ICONS.linkedIn,
+        target: '_self',
       },
       {
         url: '/blogs/5',
+        icon: ICONS.logoIcon,
+        target: '_self',
+      },
+    ],
+  },
+  {
+    name: 'Ayyubxon',
+    imgUrl: '/team/ayyubxon.jpg',
+    position: 'Software Development Engineer',
+    links: [
+      {
+        url: 'https://www.linkedin.com/in/ayyubxon-kamoldinov/',
+        icon: ICONS.linkedIn,
+        target: '_blank',
+      },
+      {
+        url: '/blogs/20',
+        icon: ICONS.logoIcon,
+        target: '_blank',
+      },
+    ],
+  },
+  {
+    name: 'Musojon',
+    imgUrl: '/team/musojon.jpg',
+    position: 'Software Development Engineer',
+    links: [
+      {
+        url: 'https://www.linkedin.com/in/musojon-tursunov/',
+        icon: ICONS.linkedIn,
+      },
+      {
+        url: '/blogs/207',
         icon: ICONS.logoIcon,
         target: '_self',
       },
