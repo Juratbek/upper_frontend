90ae92240d6bd953c64c1960306bd0648323cbb0
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-05-14 16:16:17 +0500

    974:using innerHtml in like notification

--- components/LikeNotification/LikeNotification.tsx
+++ components/LikeNotification/LikeNotification.tsx
@@ -13,7 +13,11 @@ export const LikeNotification: FC<INotificationComponentProp> = (props) => {
     <Link href={`/articles/${article.id}`}>
       <div className={rootClassName} onClick={clickHandler}>
         <div>
-          <strong className='pointer'>&quot;{article.title}&quot;</strong> maqolangiz{' '}
+          <strong
+            className='pointer'
+            dangerouslySetInnerHTML={{ __html: `&quot;${article.title}&quot;` }}
+          />{' '}
+          maqolangiz{' '}
           <Link href={`/blogs/${author.id}`} className='link'>
             <strong className='pointer'>{author.name}ga</strong>
           </Link>{' '}
