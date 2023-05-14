f7a4a210c8873de1b1007a8973dd8502811ee404
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-05-14 13:52:27 +0500

    948:fixing telegram username

--- utils/blog.ts
+++ utils/blog.ts
@@ -16,21 +16,20 @@ const LINK_DOMAINS: Partial<{ [name in TIcon]: string }> = {
 
 export const addLinkPrefix = (linkObject: ILink): string => {
   const { link, type } = linkObject;
-  let checkedLink = '';
-  if (type === 'telegram') checkedLink = link.replace('@', '');
-  else checkedLink = link;
-  if (checkedLink.startsWith('t.me')) return `https://${checkedLink}`;
+  // if link starts with http, https or // it should already be valid
+  if (link.startsWith(https) || link.startsWith(http) || link.startsWith('//')) return link;
 
-  if (checkedLink.startsWith(https) || checkedLink.startsWith(http) || checkedLink.startsWith('//'))
-    return checkedLink;
+  // if link is telegram username check its validity
+  if (type === 'telegram') {
+    if (link.startsWith('@')) return `https://${LINK_DOMAINS[type]}/${link.replace('@', '')}`;
+    if (link.startsWith('t.me')) return `https://${link}`;
+  }
+
   const domain = LINK_DOMAINS[type];
-
   if (domain)
-    return checkedLink.startsWith('/')
-      ? `https://${domain}${checkedLink}`
-      : `https://${domain}/${checkedLink}`;
+    return link.startsWith('/') ? `https://${domain}${link}` : `https://${domain}/${link}`;
 
-  return `//${checkedLink}`;
+  return `//${link}`;
 };
 
 export const convertBlogToHeadProp = (blog: IBlog): IHeadProps => {
