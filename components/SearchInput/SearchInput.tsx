066ab72ff8da239dd891c9b2c55224df00d1006f
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-05-16 18:00:55 +0500

    juratbek:outline color for search input

--- components/SearchInput/SearchInput.tsx
+++ components/SearchInput/SearchInput.tsx
@@ -1,5 +1,6 @@
 import { useDebounce, useTheme } from 'hooks';
 import { ChangeEvent, forwardRef, useEffect, useState } from 'react';
+import { getClassName } from 'utils';
 import { ICONS } from 'variables';
 
 import classes from './SearchInput.module.scss';
@@ -12,8 +13,10 @@ export const SearchInput = forwardRef<HTMLInputElement, ISearchInputProps>(funct
   ref,
 ) {
   const [value, setValue] = useState<string>(props.defaultValue as string);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
   const debauncedValue = useDebounce(value);
   const { themeColors } = useTheme();
+  const rootClassName = getClassName(classes.search, className, isFocused && classes.focused);
 
   useEffect(() => {
     onDebounce?.(debauncedValue);
@@ -26,11 +29,19 @@ export const SearchInput = forwardRef<HTMLInputElement, ISearchInputProps>(funct
   };
 
   return (
-    <div className={`${classes.search} ${className}`}>
+    <div className={rootClassName}>
       <span className={classes.icon}>
         <SearchIcon color={themeColors.icon} />
       </span>
-      <input type='text' {...props} value={value} ref={ref} onChange={changeHandler} />
+      <input
+        type='text'
+        {...props}
+        value={value}
+        ref={ref}
+        onChange={changeHandler}
+        onFocus={(): void => setIsFocused(true)}
+        onBlur={(): void => setIsFocused(false)}
+      />
     </div>
   );
 });
