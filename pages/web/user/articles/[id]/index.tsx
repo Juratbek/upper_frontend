d42b675333b0044cdf1190f21d0794b687411940
Musojon <musojontursunov925@gmail.com>
2023-05-24 19:06:27 +0500

    1056:enhancement/focus on article when load page

--- pages/web/user/articles/[id]/index.tsx
+++ pages/web/user/articles/[id]/index.tsx
@@ -59,6 +59,7 @@ export default function UserArticlePage(): JSX.Element {
 
     return (
       <Editor
+        autoFocus={!blocks.length}
         content={{
           blocks: blocks,
         }}
