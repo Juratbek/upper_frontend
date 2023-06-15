ed7ce75663cd1a2328e6890a48acac15bb79aece
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-06-15 16:51:19 +0500

    juratbek:tests for common utils

--- __tests__/utils/common.test.js
+++ __tests__/utils/common.test.js
@@ -1,4 +1,4 @@
-import { getClassName } from '../../utils/common';
+import { getClassName, isClientSide, get } from '../../utils/common';
 
 describe('getClassName', () => {
   it('combines classnames', () => {
@@ -9,5 +9,24 @@ describe('getClassName', () => {
   it('combines classnames confitionally', () => {
     const className = getClassName('m-1', 'p-1', true && 'pt-2', false && 'pb-3');
     expect(className).toEqual('m-1 p-1 pt-2');
+  });
+});
+
+describe('isClientSide', () => {
+  it('runs in the browser environment', () => {
+    const isClient = isClientSide();
+    expect(isClient).toEqual(true);
+  });
+});
+
+describe('get', () => {
+  const mock = { name: 'Aziz', age: 24, address: { city: 'Tashkent' } };
+  it('gets inner field of an object', () => {
+    const city = get(mock, 'address.city');
+    expect(city).toEqual('Tashkent');
+  });
+  it('returns undefined if field does not exist', () => {
+    const city = get(mock, 'blog.name');
+    expect(city).toEqual(undefined);
   });
 });
