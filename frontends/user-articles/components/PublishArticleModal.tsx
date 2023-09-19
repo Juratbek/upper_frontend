a061b40a9719bce4631b897161b68b6a474c4fcc
dostonabdusattorov <webdeveloperdoston@gmail.com>
2023-09-19 13:44:57 +0500

    input form removed from publish modal and notification will be sended by default

--- frontends/user-articles/components/PublishArticleModal.tsx
+++ frontends/user-articles/components/PublishArticleModal.tsx
@@ -9,7 +9,7 @@ import { usePublishMutation } from 'store/apis';
 import { setArticle } from 'store/states';
 import { IArticle, IResponseError, TArticleStatus } from 'types';
 import { validateArticle } from 'utils';
-import { ARTICLE_STATUSES, ICONS, WEB_APP_ROOT_DIR } from 'variables';
+import { ICONS, WEB_APP_ROOT_DIR } from 'variables';
 
 export const PublishArticleModal: FC<{
   open: boolean;
@@ -22,7 +22,6 @@ export const PublishArticleModal: FC<{
 }> = ({ editor, article, ...props }) => {
   const [alert, setAlert] = useState<string>();
   const { themeColors, theme } = useTheme();
-  const [isNotificationOn, setIsNotificationOn] = useState<boolean>(true);
   const dispatch = useAppDispatch();
   const [publishArticle, publishArticleRes] = usePublishMutation();
   const publishBtnRef = useRef<HTMLButtonElement>(null);
@@ -52,7 +51,7 @@ export const PublishArticleModal: FC<{
     let res;
     try {
       await props.save();
-      res = await publishArticle({ id: article.id, notificationsOn: isNotificationOn }).unwrap();
+      res = await publishArticle({ id: article.id, notificationsOn: true }).unwrap();
     } catch (e) {
       const error = e as IResponseError;
       return setAlert(error.data.message);
@@ -64,12 +63,6 @@ export const PublishArticleModal: FC<{
     props.close();
     setAlert('');
     publishArticleRes.reset();
-  };
-
-  const notificationRadioInputChangeHandler = (event: ChangeEvent<HTMLInputElement>): void => {
-    const value = event.target?.value;
-    if (value === 'false') setIsNotificationOn(false);
-    if (value === 'true') setIsNotificationOn(true);
   };
 
   useEffect(() => {
@@ -105,32 +98,6 @@ export const PublishArticleModal: FC<{
             ? 'Maqolangizni nashr qilmoqchimisiz?'
             : 'Maqolangizni qayta nashr qilmoqchimisiz?'}
         </h3>
-        {props.status === ARTICLE_STATUSES.SAVED && (
-          <div className='form-element'>
-            <p>Obunachilar maqola nashr qilingani haqida habar olishlarini hohlaysizmi?</p>
-            <div className='d-flex justify-content-center'>
-              <div className='d-flex me-2'>
-                <Input
-                  type='radio'
-                  name='notificationOn'
-                  value='true'
-                  defaultChecked
-                  onChange={notificationRadioInputChangeHandler}
-                />
-                <label className='ms-1'>Ha, albatta</label>
-              </div>
-              <div className='d-flex'>
-                <Input
-                  type='radio'
-                  name='notificationOn'
-                  value='false'
-                  onChange={notificationRadioInputChangeHandler}
-                />
-                <label className='ms-1'>Yo&apos;q</label>
-              </div>
-            </div>
-          </div>
-        )}
         <div className='d-flex'>
           <Button color='outline-dark' onClick={closePublishModalHandler} className='me-1'>
             Modalni yopish
