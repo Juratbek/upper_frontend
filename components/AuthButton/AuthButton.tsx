49475540566cc8c9c483d8d4383d96c94a69a82a
juratbek <macbookpro@Juratbek-Mac-2.local>
2023-09-27 18:17:39 +0500

    main:closing auth modal on authenticate

--- components/AuthButton/AuthButton.tsx
+++ components/AuthButton/AuthButton.tsx
@@ -1,17 +1,21 @@
 import { useAuth } from 'hooks';
 import { FC, useEffect, useMemo } from 'react';
+import { useAppDispatch } from 'store';
 import { IBlogRegisterResponse } from 'store/apis';
+import { closeAuthModal } from 'store/states';
 
 import { TRUSTED_ORIGINS } from './AuthButton.constants';
 import { IAuthButtonProps } from './AuthButton.types';
 
 export const AuthButton: FC<IAuthButtonProps> = ({ type = 'signIn', ...props }) => {
   const { authenticate } = useAuth();
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     const listener = (event: MessageEvent<IBlogRegisterResponse>): void => {
       if (TRUSTED_ORIGINS.includes(event.origin) && event.data?.token) {
         authenticate(event.data);
+        dispatch(closeAuthModal());
       }
     };
 
