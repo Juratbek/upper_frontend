7d1bc3642304931f5ad04ea4d5e6cdb2c28afa95
juratbek <juratbektfi1998@gmail.com>
2023-10-25 19:01:46 +0500

    beta:adding href for profile popover

--- components/shared/profile/components/popover/menu-list/MenuList.constants.ts
+++ components/shared/profile/components/popover/menu-list/MenuList.constants.ts
@@ -6,18 +6,22 @@ export const MENU_LIST_ITEMS: IMenuListItem[] = [
   {
     icon: ICONS.write,
     text: 'Maqolalaringiz',
+    href: '/user/articles',
   },
   {
     icon: ICONS.save,
     text: 'Saqlangan maqolalar',
+    href: '/saved-articles',
   },
   {
     icon: ICONS.notification,
     text: 'Bildirishnomalar',
+    href: '/notifications',
   },
   {
     icon: ICONS.settings,
     text: 'Sozlamalar',
+    href: '/settings',
   },
   {
     icon: ICONS.loon,
--- components/shared/profile/components/popover/menu-list/MenuList.tsx
+++ components/shared/profile/components/popover/menu-list/MenuList.tsx
@@ -1,15 +1,18 @@
+import { Link } from 'components/lib';
+import { useCallback } from 'react';
 import { ICONS } from 'variables';
 
 import { MENU_LIST_ITEMS } from './MenuList.constants';
 import classes from './MenuList.module.scss';
+import { IMenuListItem } from './MenuList.types';
 
 const NextIcon = ICONS.next;
 
 export const MenuList = (): JSX.Element => {
-  return (
-    <ul className={classes.list}>
-      {MENU_LIST_ITEMS.map((item) => (
-        <li key={item.text} className={`${classes.item} pointer`}>
+  const renderItem = useCallback((item: IMenuListItem, className: string) => {
+    if (item.href) {
+      return (
+        <Link href={item.href} className={className}>
           <span className={classes.icon}>
             <item.icon width={24} height={24} />
           </span>
@@ -17,7 +20,27 @@ export const MenuList = (): JSX.Element => {
           <span className={classes['next-icon']}>
             <NextIcon />
           </span>
-        </li>
+        </Link>
+      );
+    }
+
+    return (
+      <span className={className}>
+        <span className={classes.icon}>
+          <item.icon width={24} height={24} />
+        </span>
+        <p className={classes.text}>{item.text}</p>
+        <span className={classes['next-icon']}>
+          <NextIcon />
+        </span>
+      </span>
+    );
+  }, []);
+
+  return (
+    <ul className={classes.list}>
+      {MENU_LIST_ITEMS.map((item) => (
+        <li key={item.text}>{renderItem(item, `${classes.item} pointer`)}</li>
       ))}
     </ul>
   );
