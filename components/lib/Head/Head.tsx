327c9731279903f7f53989fef21ffa33fb310e65
Musojon <musojontursunov925@gmail.com>
2024-01-08 16:15:57 +0500

    1322:bug/replacing &nbps; with space in title

--- components/lib/Head/Head.tsx
+++ components/lib/Head/Head.tsx
@@ -12,12 +12,13 @@ export const Head: FC<IHeadProps> = (props) => {
     publishedDate,
     description = '',
   } = props;
+  const cleanedTitle = title.replaceAll(/&nbsp;/g, ' ');
 
   return (
     <NextHead>
       {/* Primary Meta Tags */}
-      <title>{title}</title>
-      <meta name='title' content={title} />
+      <title>{cleanedTitle}</title>
+      <meta name='title' content={cleanedTitle} />
       <meta name='description' content={description} />
       <meta name='author' content={author || ''} />
       <meta name='published_date' content={publishedDate || ''} />
@@ -25,7 +26,7 @@ export const Head: FC<IHeadProps> = (props) => {
       {/* Open Graph / Facebook */}
       <meta property='og:type' content='website' />
       <meta property='og:url' content={url} />
-      <meta property='og:title' content={title} />
+      <meta property='og:title' content={cleanedTitle} />
       <meta property='og:description' content={description} />
       <meta property='og:image' content={imgUrl} />
       <meta property='og:image:width' content='1200' />
@@ -36,7 +37,7 @@ export const Head: FC<IHeadProps> = (props) => {
       {/* Twitter */}
       <meta name='twitter:card' property='twitter:card' content='summary_large_image' />
       <meta property='twitter:url' content={url} />
-      <meta property='twitter:title' content={title} />
+      <meta property='twitter:title' content={cleanedTitle} />
       <meta property='twitter:description' content={description} />
       <meta property='twitter:image' content={imgUrl} />
 
