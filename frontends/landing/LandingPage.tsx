fcd546fcf0212ea1a7c91d8a5c07e3a03e505518
juratbek <juratbektfi1998@gmail.com>
2024-01-12 23:28:11 +0500

    1349: making landing page responsive for tablet and mobile

--- frontends/landing/LandingPage.tsx
+++ frontends/landing/LandingPage.tsx
@@ -1,29 +1,15 @@
-import { Divider } from 'components/lib';
-import { Labels, PublishedArticle, TopLabel } from 'components/molecules';
-import { Fragment } from 'react';
-import { usePublishedArticlesList } from 'store/clients/published-article';
-import { addAmazonBucketUrl } from 'utils/published-article';
-
 import { BlueBox } from './blue-box/BlueBox';
+import { Body } from './body/Body';
 import { LandingHeader } from './header/LandingHeader';
 
 export const LandingPage = (): JSX.Element => {
-  const { list: articles } = usePublishedArticlesList(TopLabel);
   return (
-    <div>
+    <>
       <LandingHeader />
       <main className='container' style={{ paddingTop: 48 }}>
         <BlueBox />
-        <section style={{ maxWidth: 720 }}>
-          <Labels />
-          {articles.map(addAmazonBucketUrl).map((article) => (
-            <Fragment key={article.id}>
-              <Divider color='secondary' />
-              <PublishedArticle article={article} />
-            </Fragment>
-          ))}
-        </section>
+        <Body />
       </main>
-    </div>
+    </>
   );
 };
--- frontends/landing/logos-box/LogosBox.tsx
+++ frontends/landing/logos-box/LogosBox.tsx
@@ -1,14 +1,31 @@
+import { useDevice } from 'hooks';
 import Image from 'next/image';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 
 import classes from './LogosBox.module.scss';
 
-const paths = ['vs_code.png', 'flutter.png', 'adobe.png', 'figma.png', 'chat-gpt.png', 'js.png'];
+export const LogosBox: FC = () => {
+  const { isDesktop } = useDevice();
+  const paths = useMemo(() => {
+    if (isDesktop)
+      return ['vs_code.png', 'flutter.png', 'adobe.png', 'figma.png', 'chat-gpt.png', 'js.png'];
+    return ['vs_code.png', 'chat-gpt.png', 'js.png', 'adobe.png'];
+  }, []);
 
-export const LogosBox: FC = () => (
-  <div className={classes.container}>
-    {paths.map((path) => (
-      <Image key={path} width={120} height={120} src={`/logos/${path}`} alt='vs code' />
-    ))}
-  </div>
-);
+  return (
+    <div className={classes.container}>
+      {paths.map((path, index) => (
+        <Image
+          className={classes.image}
+          key={path}
+          width={120}
+          height={120}
+          src={`/logos/${path}`}
+          alt='vs code'
+          // @ts-ignore
+          style={{ '--i': index }}
+        />
+      ))}
+    </div>
+  );
+};
