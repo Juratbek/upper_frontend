d930baea821d14946df2e4bf097951ea541af525
juratbek <juratbektfi1998@gmail.com>
2024-01-22 15:13:48 +0500

    beta:hiding article footer if editor is loading

--- frontends/article/article-page.tsx
+++ frontends/article/article-page.tsx
@@ -91,7 +91,7 @@ export const ArticlePageMain: FC<IArticlePageMainProps> = ({ article, error, ful
             onQuizSubmit={quizSubmitHandler}
           />
         </article>
-        <ArticleFooter />
+        {editorInstance?.isReady ? <ArticleFooter /> : null}
         <CommentsModal />
       </div>
     </>
