b8da9385d8eabaafcef938e5e695a407a799d5d8
juratbek <juratbektfi1998@gmail.com>
2024-01-30 13:21:36 +0500

    release:fixing typo error

--- frontends/landing/blue-box/BlueBox.tsx
+++ frontends/landing/blue-box/BlueBox.tsx
@@ -8,7 +8,7 @@ import classes from './BlueBox.module.scss';
 export const BlueBox: FC = () => {
   return (
     <div className={classes.container}>
-      <h1 className={classes.heading}>upper bilan yada yuqoriroq</h1>
+      <h1 className={classes.heading}>upper bilan yanada yuqoriroq</h1>
       <div className={classes.body}>
         <Clickable className={classes.start}>
           <span className={classes['start-text']}>Boshladik</span>
