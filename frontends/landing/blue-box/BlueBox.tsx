3a749418983bb3289b5cfb2935056a1e3fb19b72
juratbek <juratbektfi1998@gmail.com>
2024-01-31 14:38:35 +0500

    beta:opening auth site on 'Boshladik' click

--- frontends/landing/blue-box/BlueBox.tsx
+++ frontends/landing/blue-box/BlueBox.tsx
@@ -1,16 +1,19 @@
 import { UpRight } from 'components/icons/UpRight';
 import { Clickable } from 'components/lib';
+import { useAuth } from 'hooks';
 import { FC } from 'react';
 
 import { LogosBox } from '../logos-box/LogosBox';
 import classes from './BlueBox.module.scss';
 
 export const BlueBox: FC = () => {
+  const { openLoginPage } = useAuth();
+
   return (
     <div className={classes.container}>
       <h1 className={classes.heading}>upper bilan yanada yuqoriroq</h1>
       <div className={classes.body}>
-        <Clickable className={classes.start}>
+        <Clickable className={classes.start} onClick={() => openLoginPage()}>
           <span className={classes['start-text']}>Boshladik</span>
           <span className={classes['up-right-icon']}>
             <UpRight />
