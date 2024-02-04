2ec4ef07e229b2ace873ae3d1f3d42e791e236c7
juratbek <juratbektfi1998@gmail.com>
2024-02-04 23:12:24 +0500

    1389:removing line between navigation items

--- components/organisms/navigation/Navigation.tsx
+++ components/organisms/navigation/Navigation.tsx
@@ -1,6 +1,5 @@
-import { Divider } from 'components/lib';
 import { useRouter } from 'next/router';
-import { FC, Fragment } from 'react';
+import { FC } from 'react';
 import { WEB_APP_ROOT_DIR } from 'variables';
 
 import { NavItem } from './components/item';
@@ -9,17 +8,8 @@ import { INavigation } from './Navigation.types';
 export const Navigation: FC<{ items?: INavigation[] }> = ({ items = [] }) => {
   const { pathname } = useRouter();
 
-  return (
-    <>
-      {items.map((item, index) => {
-        const isActive = WEB_APP_ROOT_DIR + item.href === pathname;
-        return (
-          <Fragment key={item.text}>
-            <NavItem {...item} active={isActive} key={item.text} />
-            {index + 1 !== items.length && <Divider color='tertiary' my={4} />}
-          </Fragment>
-        );
-      })}
-    </>
-  );
+  return items.map((item) => {
+    const isActive = WEB_APP_ROOT_DIR + item.href === pathname;
+    return <NavItem {...item} active={isActive} key={item.text} />;
+  });
 };
