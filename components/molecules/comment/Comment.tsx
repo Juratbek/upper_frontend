b7d25b344523fd4d285fdfa534cfcec54637d0b8
juratbek <juratbektfi1998@gmail.com>
2024-03-23 09:28:10 +0500

    1407:fixing build error

--- components/molecules/comment/Comment.tsx
+++ components/molecules/comment/Comment.tsx
@@ -1,6 +1,5 @@
 import { Avatar, Link } from 'components/lib';
 import { FC } from 'react';
-import { addAmazonUri } from 'utils/blog/blog';
 import { dateInterval } from 'utils/date';
 
 import classes from './Comment.module.scss';
@@ -11,7 +10,7 @@ export const Comment: FC<ICommentProps> = (props) => {
 
   return (
     <div className={classes.root}>
-      <Avatar imgUrl={addAmazonUri(author).imgUrl} size='small' />
+      <Avatar imgUrl={author.imgUrl} size='small' />
       <div className={classes.body}>
         <div className={classes['body-header']}>
           <Link href={`/blogs/${author.id}`} className={classes.link}>
