bb24e065e1e9897ce3c2df3239900e1ae64c2723
juratbek <juratbektfi1998@gmail.com>
2024-04-12 14:32:55 +0300

    1423:using codemirror in code tool

--- components/molecules/editor/tools/code/Code.tool.tsx
+++ components/molecules/editor/tools/code/Code.tool.tsx
@@ -1,7 +1,38 @@
-import { FC } from 'react';
+import { defaultKeymap, history, historyKeymap } from '@codemirror/commands';
+import { javascript } from '@codemirror/lang-javascript';
+import { defaultHighlightStyle, syntaxHighlighting } from '@codemirror/language';
+import { EditorState } from '@codemirror/state';
+import { EditorView, keymap } from '@codemirror/view';
+import { FC, useEffect, useRef } from 'react';
 
 import { IToolProps } from '../tool.types';
+import { ICodeData } from './Code.types';
+import { Header } from './header/Header';
 
-export const Code: FC<IToolProps> = () => {
-  return <div>code</div>;
+export const Code: FC<IToolProps<ICodeData>> = ({ data, isEditable }) => {
+  const textareaRef = useRef<HTMLDivElement>(null);
+  const editorRef = useRef<EditorView | null>(null);
+
+  useEffect(() => {
+    editorRef.current = new EditorView({
+      parent: textareaRef.current!,
+      doc: data?.code ?? '// Salom dunyo',
+      extensions: [
+        EditorState.readOnly.of(!isEditable),
+        javascript({ jsx: true }),
+        history(),
+        keymap.of([...defaultKeymap, ...historyKeymap]),
+        syntaxHighlighting(defaultHighlightStyle),
+      ],
+    });
+
+    return () => editorRef.current!.destroy();
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <div ref={textareaRef}></div>
+    </div>
+  );
 };
---
+++ components/molecules/editor/tools/code/header/Header.tsx
@@ -0,0 +1,7 @@
+import cls from './Header.module.scss';
+
+export const Header = () => (
+  <div className={cls.header}>
+    <div className={cls['language-container']}></div>
+  </div>
+);
