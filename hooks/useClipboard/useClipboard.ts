ebe36a9e0bb91cf32398fb018f4113261a242ada
juratbek <juratbektfi1998@gmail.com>
2024-06-24 00:52:16 +0500

    1452:styling copy button for dark mode

--- hooks/useClipboard/useClipboard.ts
+++ hooks/useClipboard/useClipboard.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 
 import { IUseClipboardProps } from './useClipboard.types';
 
@@ -10,38 +10,35 @@ export const useClipboard = (config?: ICOnfig): IUseClipboardProps => {
   const [isCopied, setIsCopied] = useState(false);
   const [isError, setIsError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const timeout = useRef<NodeJS.Timeout | null>(null);
 
-  const writeText = async (text: string): Promise<void> => {
-    setIsLoading(true);
-    try {
-      await navigator.clipboard.writeText(text);
-      setIsCopied(true);
-    } catch (e) {
-      setIsError(true);
-      config?.onError?.();
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const writeText = () => {
+    let timeout: NodeJS.Timeout | undefined;
 
-  useEffect(() => {
-    if (isCopied) {
-      timeout.current = setTimeout(() => {
-        setIsCopied(false);
-        setIsError(false);
-      }, 2000);
-    }
+    return async (text: string): Promise<void> => {
+      setIsLoading(true);
+      try {
+        await navigator.clipboard.writeText(text);
+        setIsCopied(true);
 
-    return () => {
-      timeout.current && clearTimeout(timeout.current);
+        if (timeout) clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+          setIsCopied(false);
+          setIsError(false);
+        }, 2000);
+      } catch (e) {
+        setIsError(true);
+        config?.onError?.();
+      } finally {
+        setIsLoading(false);
+      }
     };
-  }, [isCopied]);
+  };
 
   return {
     isCopied,
     isError,
     isLoading,
-    writeText,
+    writeText: writeText(),
   };
 };
