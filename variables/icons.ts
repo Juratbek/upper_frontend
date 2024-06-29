00f201059886b4348b8d20c5de3fa2fd65d60bde
juratbek <juratbektfi1998@gmail.com>
2024-06-29 23:56:43 +0500

    1498:deleting unused icons

--- variables/icons.ts
+++ variables/icons.ts
@@ -4,17 +4,9 @@ import { IIconProps } from 'types';
 
 export const ICONS = {
   telegramColored: icons.TelegramColoredIcon,
-  burger: icons.BurgerIcon,
   logoIcon: icons.LogoIcon,
-  heart: icons.HeartIcon,
-  addFolder: icons.AddFolderIcon,
   plus: icons.Plus,
-  calendar: icons.CalendarIcon,
-  help: icons.HelpIcon,
-  website: icons.WebsiteIcon,
   send: icons.Send,
-  openExternal: icons.OpenExternal,
-  steps: icons.Steps,
   uploading: icons.Uploading,
   uploadError: icons.UploadError,
   uploadSuccess: icons.UploadSuccess,
