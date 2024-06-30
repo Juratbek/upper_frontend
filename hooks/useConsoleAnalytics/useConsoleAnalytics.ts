b3025433e3ea8b12c41821fd631a6dfcf1412631
juratbek <juratbektfi1998@gmail.com>
2024-06-30 22:19:14 +0500

    1503:sending gsap event on console.error

---
+++ hooks/useConsoleAnalytics/useConsoleAnalytics.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+export const useConsoleAnalytics = () => {
+  useEffect(() => {
+    window.onload = function () {
+      const originalConsoleError = console.error;
+
+      console.error = function (...args) {
+        const argsString = args.map((argument) => JSON.stringify(argument)).join(' ');
+        const message = args[0];
+
+        const { location } = window;
+
+        const error = {
+          message,
+          file: argsString,
+          path: location.pathname + location.search,
+        };
+
+        originalConsoleError(...args);
+
+        gtag('event', 'console_error', error);
+      };
+    };
+  });
+};
